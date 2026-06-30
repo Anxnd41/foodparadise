3bf654fdfbe93348b943e6533185b05e4dc5a1fd
deepanshujoshi36 <deepanshujsndelhi@gmail.com>
2026-06-30 22:58:02 +0530

    initialize web component

---
+++ web/src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./component/navbar";
+import Home from "./component/home";
+import Menu from "./component/menu";
+import PhotoGallery from "./component/photoGallery";
+import Location from "./component/location";
+import Review from "./component/review";
+import Contact from "./component/contact";
+import Footer from "./component/footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Menu />
+      <PhotoGallery />
+      <Location />
+      <Review />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
